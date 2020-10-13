550b11c82a0cc75b8498f7f1e9b29f89a82e4071
Abubakar <abubakar.i.kasule.21@dartmouth.edu>
2020-10-13 04:02:37 -0400

    99 bugs down only 1029348 to go

--- App.js
+++ App.js
@@ -2,26 +2,20 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import HomePage from './components/HomePage';
 import DocScanner from './components/DocScanner';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
-// export default function App() {
-//   return (
-//       <HomePage/>
-//   );
-// }
 
 
-
-const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
 
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomePage} />
-        <Tab.Screen name="Picture" component={DocScanner} />
-      </Tab.Navigator>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomePage} />
+        <Stack.Screen name="Picture" component={DocScanner} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
